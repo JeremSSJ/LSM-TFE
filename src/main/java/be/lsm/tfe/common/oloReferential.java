205801847e9fdea8673580a442b15446c02e7512
JeremSSJ <jeremssj7@gmail.com>
2026-05-01 15:29:58 +0200

    referential olo

---
+++ src/main/java/be/lsm/tfe/common/oloReferential.java
@@ -0,0 +1,38 @@
+package be.lsm.tfe.common;
+
+import java.util.Map;
+
+public class oloReferential {
+    Map<Integer, Double> oloMapReferential = Map.ofEntries(
+            Map.entry(1, 2.62),
+            Map.entry(2, 2.72),
+            Map.entry(3, 2.83),
+            Map.entry(4, 2.95),
+            Map.entry(5, 3.06),
+            Map.entry(6, 3.17),
+            Map.entry(7, 3.27),
+            Map.entry(8, 3.39),
+            Map.entry(9, 3.51),
+            Map.entry(10, 3.63),
+            Map.entry(11, 3.73),
+            Map.entry(12, 3.81),
+            Map.entry(13, 3.87),
+            Map.entry(14, 3.93),
+            Map.entry(15, 3.98),
+            Map.entry(16, 4.03),
+            Map.entry(17, 4.08),
+            Map.entry(18, 4.13),
+            Map.entry(19, 4.17),
+            Map.entry(20, 4.21),
+            Map.entry(21, 4.25),
+            Map.entry(22, 4.29),
+            Map.entry(23, 4.32),
+            Map.entry(24, 4.35),
+            Map.entry(25, 4.37),
+            Map.entry(26, 4.39),
+            Map.entry(27, 4.41),
+            Map.entry(28, 4.42),
+            Map.entry(29, 4.43),
+            Map.entry(30, 4.43)
+    );
+}
